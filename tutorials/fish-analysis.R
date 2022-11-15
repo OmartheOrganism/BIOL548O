6d283aa21daa4e49cb389d2a8d08b293b296d960
OmartheOrganism <omar.tariq@ubc.ca>
2022-11-15 13:55:59 -0800

    ##lecture 2

--- tutorials/fish-analysis.R
+++ tutorials/fish-analysis.R
@@ -1,8 +1,8 @@
-fish_data = read.csv("Gaeta_etal_CLC_data.csv")
+## This file reads in comma separated fish data and then appends a new column
+## describing whether the fish size is categorized as big or small (at two different thresholds)
+#fish_data = read.csv("Gaeta_etal_CLC_data.csv")
+fish_data= read.csv("C:\Users\omartari\Documents\Course Work\BIOL548O\data\Gaeta_etal_CLC_data_2.csv")
 
 library(dplyr)
-fish_data_cat = fish_data %>% 
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
-
 fish_data_cat = fish_data %>% 
   mutate(length_cat = ifelse(length > 300, "big", "small"))
