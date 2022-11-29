a245889475b7e068fa04ba2b4f4b16ddd9d25e7e
OmartheOrganism <omar.tariq@ubc.ca>
2022-11-29 13:57:00 -0800

    Completed Exercise 2.

--- scripts/lecture7-exercises.R
+++ scripts/lecture7-exercises.R
@@ -10,3 +10,24 @@ testMass <- pounds2grams(3.75)
 print(testMass)
 
 ### Exercise Two ###
+# Function: Take length of a therapod and estimate it's mass (from lecture note)
+get_mass_from_length_theropoda <- function(length){
+  mass <- 0.73 * length ^ 3.63
+  return(mass)
+}
+
+# Estimate mass of Spinosaurus
+spinosaurusMass <- get_mass_from_length_theropoda(16)
+print(spinosaurusMass)
+
+# Generate new function which takes in a and b as parameters alongside length
+get_mass_from_length <- function(length,a,b){
+  mass = a*length^b
+  return(mass)
+}
+
+# Estimate mass of a Sauropod (for which a=214.44, b=1.46) whose length is 26m
+sauropodMass <- get_mass_from_length(26,214.44,1.46)
+print(sauropodMass)
+
+### 
