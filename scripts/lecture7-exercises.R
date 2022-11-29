ef35ef393520add8b84f92f70db7fdf70091f646
OmartheOrganism <omar.tariq@ubc.ca>
2022-11-29 14:05:54 -0800

    Completed Exercise 3 & 4.

--- scripts/lecture7-exercises.R
+++ scripts/lecture7-exercises.R
@@ -30,4 +30,28 @@ get_mass_from_length <- function(length,a,b){
 sauropodMass <- get_mass_from_length(26,214.44,1.46)
 print(sauropodMass)
 
-### 
+### Exercise 3 ###
+#rewrite get_mass_from_length function to set default parameters
+## rewriting so that the code for exercise two is preserved, so long as the script is executed in order everything should be fine
+get_mass_from_length <- function(length,a=39.9,b=2.6){
+  mass = a*length^b
+  return(mass)
+}
+
+# redo calculation of Sauropod mass 
+sauropodMass <- get_mass_from_length(26,a=214.44,b=1.46)
+print(sauropodMass)
+
+# calculate mass of unknown dinosaur of length 16m 
+unknownDinoMass <- get_mass_from_length(16)
+print(unknownDinoMass)
+
+### Exercise 4 ###
+kg2pounds <- function(mass){
+  pounds = mass*2.205
+  return(pounds)
+}
+
+# get mass (in pounds) of Stegosaurus (a=10.95,b=2.64) which has length of 12m
+stegosaurusMass <- kg2pounds(get_mass_from_length(12,a=10.95,b=2.64))
+print(stegosaurusMass)
