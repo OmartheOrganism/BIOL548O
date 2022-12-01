233331d1ee2b1d125c2c14e910a5e452c5d42e0a
OmartheOrganism <omar.tariq@ubc.ca>
2022-12-01 14:25:02 -0800

    Completed Exercise 4.

--- scripts/lecture8-exercises.R
+++ scripts/lecture8-exercises.R
@@ -48,3 +48,31 @@ if (age_class == "sapling"){
 }
 print(y)
 
+### Exercise 4 ###
+get_mass_from_length_by_name <- function(length,name){
+  if (name=="Stegosauria"){
+    a <- 10.95
+    b <- 2.64
+  } else if (name=="Theropoda"){
+    a <- 0.73
+    b <- 3.63
+  } else if (name=="Sauropoda"){
+    a <- 214.44
+    b <- 1.46
+  } else {
+    print("I'm sorry I don't know how to deal with that kind of dino!")
+    a <- NA
+    b <- NA
+    mass <- NA
+  }
+  mass <- a*length^b
+  return(mass)
+}
+
+# use new fxn to get mass
+print(get_mass_from_length_by_name(10,"Stegosauria"))
+print(get_mass_from_length_by_name(8,"Theropoda"))
+print(get_mass_from_length_by_name(12,"Sauropoda"))
+print(get_mass_from_length_by_name(13,"Ankylosauria"))
+
+
