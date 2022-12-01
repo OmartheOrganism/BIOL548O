f0e19b3907e65173c395b0ea22ca835cd60232ac
OmartheOrganism <omar.tariq@ubc.ca>
2022-12-01 13:56:14 -0800

    Completed Exercise One.

---
+++ scripts/lecture8-exercises.R
@@ -0,0 +1,22 @@
+### Exercise One ###
+# Inialize variables
+w <- 10.2
+x <- 1.3
+y <- 2.8
+z <- 17.5
+colors <- c("red", "blue", "green")
+masses <- c(45.2, 36.1, 27.8, 81.6, 42.4)
+dna1 <- "attattaggaccaca"
+dna2 <- "attattaggaacaca"
+
+# Determine if:
+## w > 10?
+print(w > 10)
+## green is in colors?
+print("green" %in% colors)
+## x >y?
+print(x > y)
+## are any values in masses greater than 40?
+print("TRUE" %in% c(masses > 40))
+
+### Exercise Two ###
