6f001dae32345ac5d556e7d42365147d4e19b1d0
OmartheOrganism <omar.tariq@ubc.ca>
2022-12-07 23:22:58 -0800

    added link to other repo, sorry for confusion.

---
+++ scripts/lecture2-exercises.R
@@ -0,0 +1,6 @@
+# Because I missed the first lecture I did the work for the second lecture in a seperate repo (wasn't sure whether)
+# I should start a new repo or clone yours, the link to that work is here (you've been added as a collarboator on both).
+
+# LINK: https://github.com/OmartheOrganism/BIO548O/blob/remote/fish-analysis.R
+
+# please message me if you have any issues
